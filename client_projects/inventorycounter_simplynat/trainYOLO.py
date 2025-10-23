51c4d71c06ebce4088c0e40d43646e7700a6a736
Evanmaurer <evanm807@gmail.com>
2025-10-23 16:24:07 -0500

    added Read me to explain project

--- client_projects/inventorycounter_simplynat/trainYOLO.py
+++
@@ -1,6 +0,0 @@
-#train YOLOv8 to find the inventory on shelves from pictures from client
-#the ocr will be used to find the text on the inventory items withen the yolo boxes
-#the yolo model will count the items based on the boxes detected
-#the ocr model will read the text on the items to identify them
-# will need to label all inventory first few times so ai can be trained on data 
-#make sure data is labeled based on the inventory items names ß
