03efcdeb1309f34888c2249624121b773bb787d3
Evan maurer <evanmaurer@Evans-MacBook-Air.local>
2025-10-23 15:42:51 -0500

    Commit: stage & commit workspace changes (assistant)

---
+++ client_projects/inventorycounter_simplynat/compare_inv.py
@@ -0,0 +1,2 @@
+# compare the inv with the pulled inv from pull_current_inv.py
+# DONT UPDATE VALUES MAKE SURE PEOPLE DO THAT NOT BOT FOR NOW
---
+++ client_projects/inventorycounter_simplynat/trainYOLO.py
@@ -0,0 +1,3 @@
+#train YOLOv8 to find the inventory on shelves from pictures from client 
+# will need to label all inventory first few times so ai can be trained on data 
+#make sure data is labeled based on the inventory items names 
